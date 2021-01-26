221554db43c8b98c82bf3ae20a328910c6a29706
munozjuan <munoz.juan07@hotmail.com>
2021-01-25 23:44:06 -0500

    adde db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Chicken Bacon Ranch");
+INSERT INTO burgers (burger_name) VALUES ("Beef Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Quarter Pounder");
+
+SELECT * FROM burgers;
