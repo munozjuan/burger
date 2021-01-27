59e554eb9615b490b7860e0cab67b381a8642778
munozjuan <munoz.juan07@hotmail.com>
2021-01-26 20:30:04 -0500

    fixed js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
